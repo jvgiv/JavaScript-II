97d86c3bf8a6a045d2cde259242ff4b2c5227bae
jvgiv <jvgiv12@gmail.com>
2019-02-26 18:44:43 -0500

    all non-stretch goals complete

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,12 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+var guest = 'Garfield'
+function saysHello() {
+  console.log('hello, ' + guest + '!');
+}
+
+saysHello();
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
